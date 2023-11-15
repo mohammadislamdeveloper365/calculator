633e8c4caadbba7abb07a039202a12095d9b4282
Mohammad Moinul Islam <95491483+mohammadislamdeveloper365@users.noreply.github.com>
2023-11-15 16:28:40 +0400

    feat: add calculator functionality logic

--- js/index.js
+++ js/index.js
@@ -1,43 +1,16 @@
-
 getElementById('calculator-container').addEventListener('click', function(e) {
     if(e.target.innerText !== 'C') {
         getElementById('calculator-input').value = getElementById('calculator-input').value + e.target.innerText;
     } else {
         getElementById('calculator-input').value = '';
     }
-
-    if(e.target.innerText === '+' || e.target.innerText === '-' || e.target.innerText === 'x' || e.target.innerText === '/' ) {
-        // let number = 0;
-        // if(e.target.innerText === '+') {
-        //     number = parseInt(getElementById('calculator-input').value);
-        //     result += number;
-        // }  else if(e.target.innerText === '-') {
-        //     if(!result) {
-        //         result = parseInt(getElementById('calculator-input').value);
-        //     } else {
-        //         number = parseInt(getElementById('calculator-input').value);
-        //         result -= number;
-        //     }
-        // }  else if(e.target.innerText === 'x') {
-        //     if(!result) {
-        //         result = parseInt(getElementById('calculator-input').value);
-        //     } else {
-        //         number = parseInt(getElementById('calculator-input').value);
-        //         result *= number;
-        //     }
-            
-        // }  else {
-        //     if(!result) {
-        //         result = parseInt(getElementById('calculator-input').value);
-        //     } else {
-        //         number = parseInt(getElementById('calculator-input').value);
-        //         result /= number;
-        //     }
-        // } 
-        // getElementById('calculator-input').value = result; 
-    }
 })
 
+getElementById('calculate').addEventListener('click',function(){
+    if(getElementById('calculator-input').value) {
+       getElementById('calculator-input').value = eval(getElementById('calculator-input').value)
+    }
+})
 function getElementById(id){
     return document.getElementById(id);
 }
